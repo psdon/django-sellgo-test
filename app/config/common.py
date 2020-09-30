34630ec37aa94f56bd8edad3efb4a296766ce43b
psdon <psdonium@gmail.com>
2020-10-01 07:25:35 +0800

    added: default secret key

--- app/config/common.py
+++ app/config/common.py
@@ -39,7 +39,7 @@ class Common(Configuration):
 
     ALLOWED_HOSTS = ["*"]
     ROOT_URLCONF = "app.urls"
-    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+    SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "sample")
     WSGI_APPLICATION = "app.wsgi.application"
 
     # Email
